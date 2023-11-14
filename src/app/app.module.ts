12c069b9da0a5366c05f8eec528d378d3c1ca630
joaogabriel87 <joagabriel9@gmail.com>
2023-11-13 23:54:41 -0300

    Adição dos Cards e de um novo background

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FiltreComponent } from './components/filtre/filtre.component';
-import { DropmenuComponent } from './components/dropmenu/dropmenu.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CardComponent } from './components/card/card.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FiltreComponent,
-    DropmenuComponent
+    HomeComponent,
+    CardComponent,
+    
   ],
   imports: [
     BrowserModule,
--- src/app/components/card/card.component.spec.ts
+++ src/app/components/card/card.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { DropmenuComponent } from './dropmenu.component';
+import { CardComponent } from './card.component';
 
-describe('DropmenuComponent', () => {
-  let component: DropmenuComponent;
-  let fixture: ComponentFixture<DropmenuComponent>;
+describe('CardComponent', () => {
+  let component: CardComponent;
+  let fixture: ComponentFixture<CardComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [DropmenuComponent]
+      declarations: [CardComponent]
     });
-    fixture = TestBed.createComponent(DropmenuComponent);
+    fixture = TestBed.createComponent(CardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
